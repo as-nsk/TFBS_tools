cbeb9a2afddd8983d9d28520dcd7eb73ac5c49bc
Arthur <arthur@gmail.com>
2019-03-10 18:23:58 +0700

    Starting doing the interface

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QHBoxLayout, QLabel, QApplication)
+from PyQt5.QtGui import QPixmap
 
-def main():
-    a = 1
-    b = 3
-    sum = a + b
-    print('hello world')
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        hbox = QHBoxLayout(self)
+        pixmap = QPixmap("logo.jpg")
+        lbl = QLabel(self)
+        lbl.setPixmap(pixmap)
+        hbox.addWidget(lbl)
+        self.setLayout(hbox)
+
+        self.move(100, 200)
+        self.setWindowTitle('Red Rock')
+        self.show()
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
