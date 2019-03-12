9e07e9eaf08c4ff86dc287b91e9bf4962b757a99
Arthur <arthur@gmail.com>
2019-03-12 10:19:45 +0700

    MainWindow .ui -> main.py

--- main.py
+++ main.py
@@ -1,28 +1,83 @@
-import sys
-from PyQt5.QtWidgets import (QWidget, QHBoxLayout, QLabel, QApplication)
-from PyQt5.QtGui import QPixmap
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MainWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.12
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Example(QWidget):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(746, 541)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(90, 10, 541, 201))
+        self.graphicsView.setObjectName("graphicsView")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(320, 310, 61, 16))
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(90, 260, 171, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(430, 230, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(90, 300, 141, 21))
+        self.label_3.setObjectName("label_3")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(550, 420, 75, 23))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(90, 230, 61, 16))
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(330, 230, 71, 16))
+        self.label_5.setObjectName("label_5")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(270, 260, 171, 23))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(90, 330, 171, 23))
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_6.setGeometry(QtCore.QRect(90, 360, 171, 23))
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_7.setGeometry(QtCore.QRect(90, 390, 171, 23))
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.pushButton_8 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_8.setGeometry(QtCore.QRect(90, 420, 171, 23))
+        self.pushButton_8.setObjectName("pushButton_8")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 746, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def initUI(self):
-        hbox = QHBoxLayout(self)
-        pixmap = QPixmap("logo.jpg")
-        lbl = QLabel(self)
-        lbl.setPixmap(pixmap)
-        hbox.addWidget(lbl)
-        self.setLayout(hbox)
-
-        self.move(100, 200)
-        self.setWindowTitle('TFBS Analysis Portable 1.0')
-        self.show()
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Parametres"))
+        self.pushButton.setText(_translate("MainWindow", "Load data from working folder"))
+        self.pushButton_2.setText(_translate("MainWindow", "Open"))
+        self.label_3.setText(_translate("MainWindow", "Output"))
+        self.pushButton_3.setText(_translate("MainWindow", "Help"))
+        self.label_4.setText(_translate("MainWindow", "Input"))
+        self.label_5.setText(_translate("MainWindow", "Working folder:"))
+        self.pushButton_4.setText(_translate("MainWindow", "Load data from database"))
+        self.pushButton_5.setText(_translate("MainWindow", "Load data from working folder"))
+        self.pushButton_6.setText(_translate("MainWindow", "Load data from working folder"))
+        self.pushButton_7.setText(_translate("MainWindow", "Load data from working folder"))
+        self.pushButton_8.setText(_translate("MainWindow", "Load data from working folder"))
