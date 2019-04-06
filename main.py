2fde8a2fcbfce19af5b8cb88e7f2f5bf7bcc33e7
Arthur <arthur@gmail.com>
2019-04-06 15:12:32 +0700

    Buttons 1,2 work

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import os
 import sys
 from PyQt5 import QtWidgets
 from MainWindow import Ui_MainWindow
@@ -12,17 +12,26 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.LoadLocalDataButton.clicked.connect(self.onPushButtonClick)
+        self.ui.BuildClustersButton.clicked.connect(self.BuildClustersClick)
         self.show()
 
+        self.chosen_files = []
+
     def onPushButtonClick(self):
-        import os
+        print("test")
+
         directory = os.getcwd()
 
-
         fname = QFileDialog.getOpenFileNames(self, 'Open file', directory)
-        print(fname[0])
-        self.chosenFiles = fname[0]
-    
+        # print(fname[0])
+        self.chosen_files = fname[0]
+
+    def BuildClustersClick(self):
+        print('here we will build clusters')
+        if self.chosen_files:
+            print(self.chosen_files)
+        else:
+            print('no files were chosen')
 
 
 if __name__ == '__main__':
